khanwul/EZ2CV

fix: modify to include difficulty levels in config & extraction results

src/ez2cv/config.py
@@ -107,6 +107,7 @@ class RunConfig:
     """Resolved settings used only by video decoding and detection."""
     # --- provenance ----------------------------------------------------------
     song_name: str
+    difficulty: str
     skin_name: str
     key_mode: str
     display_resolution: tuple[int, int]
@@ -151,7 +152,7 @@ class RunConfig:
     min_longnote_px: int                    # head-tail gap below this -> tap
 
     def summary(self) -> None:
-        print(f"=== Config: {self.skin_name} / {self.key_mode} / "
+        print(f"=== Config: {self.skin_name} / {self.key_mode} / {self.difficulty} / "
               f"{self.display_resolution[0]}x{self.display_resolution[1]} ===")
         print(f"  video      : {self.video_path}")
         print(f"  fps/speed  : {self.fps} / {self.note_speed}   "
@@ -209,6 +210,10 @@ def load_config(song_toml_path: str | Path) -> RunConfig:
     setup       = song["setup"]
     skin_name   = setup["skin"]
     key_mode    = str(setup["key_mode"]).strip().lower()
+    difficulty  = str(setup["difficulty"]).strip().upper()
+    if difficulty not in {"EZ", "NM", "HD", "SHD"}:
+        raise ConfigError(
+            f"setup.difficulty must be EZ, NM, HD, or SHD (got {difficulty!r})")
     res_str     = setup["display_resolution"]
 
     skin_path    = config_root / "skins" / skin_name / "skin.toml"
@@ -408,6 +413,7 @@ def load_config(song_toml_path: str | Path) -> RunConfig:
 
     return RunConfig(
         song_name=song_name,
+        difficulty=difficulty,
         skin_name=skin_name,
         key_mode=key_mode,
         display_resolution=prof_res,

tests/test_raw_checkpoint.py
@@ -21,6 +21,7 @@ from ez2cv.io import (read_chart, read_raw, serialize_chart, serialize_raw,
 def _raw_chart() -> RawChart:
     return RawChart(
         song_name="demo",
+        difficulty="SHD",
         skin_name="ez2on",
         key_mode="5k",
         lane_colors=("white", "cyan", "white", "cyan", "white"),
@@ -67,6 +68,7 @@ class RawCheckpointTest(unittest.TestCase):
         self.assertEqual(loaded, raw)
         chart = build_chart(loaded)
         self.assertEqual(chart.song_name, "demo")
+        self.assertEqual(chart.difficulty, "SHD")
         self.assertEqual(len(chart.notes), 2)
         self.assertEqual(chart.notes[0].start_tick, 192)
         self.assertEqual(chart.notes[1].end_tick, 576)
@@ -116,7 +118,8 @@ class RawCheckpointTest(unittest.TestCase):
 
     def test_checkpoint_survives_chart_failure(self):
         raw = _raw_chart()
-        config = SimpleNamespace(song_name="demo", summary=lambda: None)
+        config = SimpleNamespace(song_name="demo", difficulty="SHD",
+                                 summary=lambda: None)
         with tempfile.TemporaryDirectory() as temp_dir, chdir(temp_dir), \
                 patch("ez2cv.cli.load_config", return_value=config), \
                 patch("ez2cv.cli.DetectionPipeline") as pipeline, \
@@ -125,7 +128,7 @@ class RawCheckpointTest(unittest.TestCase):
             with self.assertRaisesRegex(RuntimeError, "boom"):
                 with redirect_stdout(StringIO()):
                     run("ignored.toml", chart_image=False, progress=False)
-            self.assertTrue(Path("out/demo/demo_raw.json").is_file())
+            self.assertTrue(Path("out/demo/SHD/demo_raw.json").is_file())
 
     def test_v3_chart_round_trip_and_meter_events(self):
         chart = build_chart(_raw_chart())
@@ -136,7 +139,8 @@ class RawCheckpointTest(unittest.TestCase):
 
         payload = serialize_chart(chart)
         self.assertEqual((payload["format"], payload["version"]),
-                         ("ez2cv.chart", "3.0"))
+                         ("ez2cv.chart", "3.1"))
+        self.assertEqual(payload["meta"]["difficulty"], "SHD")
         self.assertEqual(payload["timing"]["meter_events"], [
             {"start_tick": 0, "numerator": 4, "denominator": 4},
             {"start_tick": 768, "numerator": 3, "denominator": 4},

khanwul/EZ2CV-djmax

src/ez2cv/config.py
@@ -123,6 +123,7 @@ class RunConfig:
     """Resolved settings used only by video decoding and detection."""
     # --- provenance ----------------------------------------------------------
     song_name: str
+    difficulty: str
     skin_name: str
     key_mode: str
     display_resolution: tuple[int, int]
@@ -197,7 +198,7 @@ class RunConfig:
         return self._normal_lane().min_longnote_px
 
     def summary(self) -> None:
-        print(f"=== Config: {self.skin_name} / {self.key_mode} / "
+        print(f"=== Config: {self.skin_name} / {self.key_mode} / {self.difficulty} / "
               f"{self.display_resolution[0]}x{self.display_resolution[1]} ===")
         print(f"  video      : {self.video_path}")
         print(f"  fps/speed  : {self.fps} / {self.note_speed}   "
@@ -256,6 +257,10 @@ def load_config(song_toml_path: str | Path) -> RunConfig:
     setup       = song["setup"]
     skin_name   = setup["skin"]
     key_mode    = str(setup["key_mode"]).strip().lower()
+    difficulty  = str(setup["difficulty"]).strip().upper()
+    if difficulty not in {"NM", "HD", "MX", "SC"}:
+        raise ConfigError(
+            f"setup.difficulty must be NM, HD, MX, or SC (got {difficulty!r})")
     res_str     = setup["display_resolution"]
 
     skin_path    = config_root / "skins" / skin_name / "skin.toml"
@@ -536,6 +541,7 @@ def load_config(song_toml_path: str | Path) -> RunConfig:
 
     return RunConfig(
         song_name=song_name,
+        difficulty=difficulty,
         skin_name=skin_name,
         key_mode=key_mode,
         display_resolution=prof_res,

tests/test_raw_checkpoint.py
@@ -21,6 +21,7 @@ from ez2cv.io import (read_chart, read_raw, serialize_chart, serialize_raw,
 def _raw_chart() -> RawChart:
     return RawChart(
         song_name="demo",
+        difficulty="SC",
         skin_name="ONGEKI",
         key_mode="5b",
         tracks=(
@@ -74,6 +75,7 @@ class RawCheckpointTest(unittest.TestCase):
         self.assertEqual(loaded.tracks[0].role, "overlay")
         chart = build_chart(loaded)
         self.assertEqual(chart.song_name, "demo")
+        self.assertEqual(chart.difficulty, "SC")
         self.assertEqual(len(chart.notes), 2)
         self.assertEqual(chart.notes[0].start_tick, 192)
         self.assertEqual(chart.notes[1].end_tick, 576)
@@ -123,7 +125,8 @@ class RawCheckpointTest(unittest.TestCase):
 
     def test_checkpoint_survives_chart_failure(self):
         raw = _raw_chart()
-        config = SimpleNamespace(song_name="demo", summary=lambda: None)
+        config = SimpleNamespace(song_name="demo", difficulty="SC",
+                                 summary=lambda: None)
         with (
             tempfile.TemporaryDirectory() as temp_dir,
             chdir(temp_dir),
@@ -135,7 +138,7 @@ class RawCheckpointTest(unittest.TestCase):
             with self.assertRaisesRegex(RuntimeError, "boom"):
                 with redirect_stdout(StringIO()):
                     run("ignored.toml", chart_image=False, progress=False)
-            self.assertTrue(Path("out/demo/demo_raw.json").is_file())
+            self.assertTrue(Path("out/demo/SC/demo_raw.json").is_file())
 
     def test_chart_keeps_notes_before_first_observed_barline(self):
         raw = _raw_chart()
@@ -158,7 +161,8 @@ class RawCheckpointTest(unittest.TestCase):
 
         payload = serialize_chart(chart)
         self.assertEqual((payload["format"], payload["version"]),
-                         ("ez2cv.chart", "3.0"))
+                         ("ez2cv.chart", "3.1"))
+        self.assertEqual(payload["meta"]["difficulty"], "SC")
         self.assertEqual(payload["timing"]["meter_events"], [
             {"start_tick": 0, "numerator": 4, "denominator": 4},
             {"start_tick": 768, "numerator": 3, "denominator": 4},