khanwul/EZ2CV

fix: improve timing inference and long-note integrity
  - unify TickClock conversion and serialized BPM segments
  - repair missing and false beats during timeline inference
  - preserve long notes when tail quantization is uncertain
  - add adaptive head and tail grid snapping
  - track separate head and tail timing uncertainty
  - use container PTS and validate video calibration
  - normalize translated playfields from the judgment band
  - preserve provenance and per-note diagnostics
  - add synthetic end-to-end and regression tests

src/ez2cv/io.py
@@ -15,11 +15,11 @@ from ez2cv.detection.beat import BeatEvent
 from ez2cv.detection.tracking import RawNote
 
 
-RAW_SCHEMA_VERSION = "2.2"
-RAW_READABLE_VERSIONS = {"2.1", "2.2"}
+RAW_SCHEMA_VERSION = "2.3"
+RAW_READABLE_VERSIONS = {"2.1", "2.2", "2.3"}
 CHART_FORMAT = "ez2cv.chart"
-CHART_VERSION = "3.2"
-CHART_READABLE_VERSIONS = {"3.1", "3.2"}
+CHART_VERSION = "3.3"
+CHART_READABLE_VERSIONS = {"3.1", "3.2", "3.3"}
 
 
 def _reject_constant(value: str) -> None:
@@ -29,7 +29,7 @@ def _reject_constant(value: str) -> None:
 def _ms(value: float) -> float:
     if not math.isfinite(value):
         raise ValueError(f"non-finite ms value: {value}")
-    return round(float(value), 3)
+    return float(value)
 
 
 def _tick(value: int) -> int:
@@ -69,6 +69,7 @@ def serialize_raw(raw: RawChart) -> dict:
             "frame_count": int(raw.frame_count),
             "duration_ms": _ms(raw.duration_ms),
             "orphan_tails": int(raw.orphan_tails),
+            "provenance": raw.provenance,
         },
         "notes": [
             {
@@ -79,7 +80,10 @@ def serialize_raw(raw: RawChart) -> dict:
                 "end_ms": _ms(note.end_ms) if note.end_ms is not None else None,
                 "confidence": round(float(note.confidence), 4),
                 "extrapolated": bool(note.extrapolated),
-                "timing_sigma_ms": _ms(note.timing_sigma_ms),
+                "start_sigma_ms": _ms(note.start_sigma_ms),
+                "end_sigma_ms": (_ms(note.end_sigma_ms)
+                                 if note.end_sigma_ms is not None else None),
+                "pairing_status": note.pairing_status,
             }
             for note in raw.notes
         ],
@@ -139,6 +143,7 @@ def read_raw(path: str | Path) -> RawChart:
             max_bpm=float(meta["max_bpm"]),
             frame_count=int(meta["frame_count"]),
             orphan_tails=int(meta["orphan_tails"]),
+            provenance=dict(meta.get("provenance", {})),
             notes=[RawNote(
                 lane=int(note["lane"]),
                 type=str(note["type"]),
@@ -148,7 +153,14 @@ def read_raw(path: str | Path) -> RawChart:
                 color=str(note["color"]),
                 confidence=float(note["confidence"]),
                 extrapolated=bool(note["extrapolated"]),
-                timing_sigma_ms=float(note.get("timing_sigma_ms", 0.0)),
+                start_sigma_ms=float(note.get(
+                    "start_sigma_ms", note.get("timing_sigma_ms", 0.0))),
+                end_sigma_ms=(
+                    None if note["end_ms"] is None
+                    else float(note["end_sigma_ms"])
+                    if note.get("end_sigma_ms") is not None
+                    else 0.0 if "end_sigma_ms" not in note else None),
+                pairing_status=str(note.get("pairing_status", "observed")),
             ) for note in payload["notes"]],
             beats=[BeatEvent(
                 frame_index=int(beat["frame_index"]),
@@ -168,12 +180,14 @@ def read_raw(path: str | Path) -> RawChart:
     numeric = [raw.fps, raw.note_speed, raw.min_bpm, raw.max_bpm]
     numeric.extend(note.trigger_ms for note in raw.notes)
     numeric.extend(note.end_ms for note in raw.notes if note.end_ms is not None)
-    numeric.extend(note.timing_sigma_ms for note in raw.notes)
+    numeric.extend(note.start_sigma_ms for note in raw.notes)
+    numeric.extend(note.end_sigma_ms for note in raw.notes
+                   if note.end_sigma_ms is not None)
     numeric.extend(beat.ms for beat in raw.beats)
     numeric.extend(barline.ms for barline in raw.barlines)
     if not all(math.isfinite(value) for value in numeric):
         raise ValueError(f"invalid raw chart {source}: non-finite number")
-    if raw.fps <= 0 or raw.tick_resolution <= 0 or not raw.lane_colors:
+    if raw.fps <= 0 or raw.tick_resolution != 192 or not raw.lane_colors:
         raise ValueError(f"invalid raw chart {source}: invalid timing or lanes")
     if raw.min_bpm <= 0 or raw.max_bpm < raw.min_bpm:
         raise ValueError(f"invalid raw chart {source}: invalid BPM range")
@@ -183,8 +197,14 @@ def read_raw(path: str | Path) -> RawChart:
         raise ValueError(f"invalid raw chart {source}: note lane out of range")
     if any(note.type not in {"tap", "longnote"} for note in raw.notes):
         raise ValueError(f"invalid raw chart {source}: unknown note type")
-    if any(note.timing_sigma_ms < 0 for note in raw.notes):
+    if any(note.start_sigma_ms < 0 or (
+            note.end_sigma_ms is not None and note.end_sigma_ms < 0)
+            for note in raw.notes):
         raise ValueError(f"invalid raw chart {source}: negative timing sigma")
+    if any(note.pairing_status not in {
+            "observed", "inferred_tail", "unclosed_head", "short_pair"}
+            for note in raw.notes):
+        raise ValueError(f"invalid raw chart {source}: invalid pairing status")
     if any(note.type == "longnote" and (
             note.end_ms is None or note.end_ms <= note.trigger_ms)
             for note in raw.notes):
@@ -202,6 +222,7 @@ def _note_payload(note: ChartNote, note_id: int) -> dict:
         "start_tick": _tick(note.start_tick),
         "end_tick": _tick(note.end_tick) if note.end_tick is not None else None,
         "off_grid": bool(note.off_grid),
+        "needs_review": bool(note.needs_review),
     }
 
 
@@ -211,11 +232,11 @@ def _bpm_segment_payload(segment: BPMSegment) -> dict:
         "end_tick": _tick(segment.end_tick),
     }
     if segment.is_constant:
-        out.update(bpm=round(float(segment.bpm_start), 4),
+        out.update(bpm=float(segment.bpm_start),
                    interpolation="step")
     else:
-        out.update(bpm_start=round(float(segment.bpm_start), 4),
-                   bpm_end=round(float(segment.bpm_end), 4),
+        out.update(bpm_start=float(segment.bpm_start),
+                   bpm_end=float(segment.bpm_end),
                    interpolation="linear_time")
     return out
 
@@ -266,7 +287,7 @@ def serialize_chart(chart: Chart) -> dict:
         "notes": [
             _note_payload(note, note_id)
             for note_id, note in enumerate(chart.notes)],
-        "analysis": {"stats": chart.stats},
+        "analysis": {"stats": chart.stats, "notes": chart.diagnostics},
     }
 
 
@@ -317,6 +338,8 @@ def read_chart(path: str | Path) -> dict:
             if meter["numerator"] <= 0 or meter["denominator"] <= 0:
                 raise ValueError("invalid meter event")
         for note in notes:
+            if chart["version"] in {"3.1", "3.2"}:
+                note.setdefault("needs_review", False)
             if not 0 <= note["lane"] < lane_count:
                 raise ValueError("note lane out of range")
             if (note["end_tick"] is not None
@@ -324,6 +347,8 @@ def read_chart(path: str | Path) -> dict:
                 raise ValueError("invalid longnote end")
             if not isinstance(note["off_grid"], bool):
                 raise ValueError("off_grid must be boolean")
+            if not isinstance(note["needs_review"], bool):
+                raise ValueError("needs_review must be boolean")
         numeric = [meta["duration_ms"], timing["tick_zero_ms"]]
         if not all(math.isfinite(float(value)) for value in numeric):
             raise ValueError("non-finite chart metadata")

tests/synthetic_video.py
@@ -1,0 +1,127 @@
+"""Procedural, copyright-free video fixture for the public end-to-end test."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+from ez2cv.config import LaneConfig, MeasureLineConfig, RunConfig
+
+
+FPS = 60.0
+SPEED = 4.0
+TRIGGER = 120
+LINE_Y = 130
+NOTE_HEIGHT = 10
+LANES = ((60, 90), (90, 120))
+BEAT_FRAMES = tuple(range(20, 261, 30))
+BARLINE_FRAMES = (20, 140, 260)
+
+
+def _sprite(kind: str) -> np.ndarray:
+    image = np.full((NOTE_HEIGHT, 30, 3), 180, dtype=np.uint8)
+    if kind == "note":
+        image[:, 12:18] = 250
+        image[2:8, 14:16] = 40
+    elif kind == "lnhead":
+        image[:, 4:10] = 245
+        image[2:8, 6:8] = 30
+    else:
+        image[:, 20:26] = 245
+        image[2:8, 22:24] = 30
+    return image
+
+
+def _blit(frame: np.ndarray, sprite: np.ndarray, x: int, y: int) -> None:
+    h, w = sprite.shape[:2]
+    if 0 <= y <= frame.shape[0] - h:
+        frame[y:y + h, x:x + w] = sprite
+
+
+def make_fixture(path: Path, alignment_offset=(0, 0)) -> tuple[RunConfig, dict]:
+    align_x, align_y = alignment_offset
+    templates = {kind: _sprite(kind) for kind in ("note", "lnhead", "lntail")}
+    writer = cv2.VideoWriter(
+        str(path), cv2.VideoWriter_fourcc(*"FFV1"), FPS, (200, 160))
+    if not writer.isOpened():
+        raise RuntimeError("FFV1 video writer unavailable")
+
+    taps = ((0, 50), (1, 80), (1, 230))
+    longnote = (0, 110, 200)       # lane, head crossing, release frame
+    try:
+        for frame_index in range(285):
+            frame = np.zeros((160, 200, 3), dtype=np.uint8)
+            frame[140 + align_y:145 + align_y,
+                  LANES[0][0] + align_x:LANES[-1][1] + align_x + 1, 0] = 220
+            if frame_index - 1 in BEAT_FRAMES:
+                frame[10 + align_y:20 + align_y,
+                      10 + align_x:20 + align_x] = 255
+            for crossing in BARLINE_FRAMES:
+                y = int(round(LINE_Y + SPEED * (frame_index - crossing)))
+                if 10 <= y < 150:
+                    frame[y + align_y,
+                          LANES[0][0] + align_x:
+                          LANES[-1][1] + align_x] = 80
+            for lane, crossing in taps:
+                y = int(round(TRIGGER + SPEED * (frame_index - crossing)))
+                _blit(frame, templates["note"],
+                      LANES[lane][0] + align_x, y + align_y)
+
+            lane, head_crossing, release = longnote
+            tail_crossing = release - NOTE_HEIGHT / SPEED
+            head_y = int(round(TRIGGER + SPEED * (frame_index - head_crossing)))
+            tail_y = int(round(TRIGGER + SPEED * (frame_index - tail_crossing)))
+            top, bottom = max(10, tail_y), min(150, head_y + NOTE_HEIGHT)
+            if top < bottom:
+                frame[top + align_y:bottom + align_y,
+                      LANES[lane][0] + align_x:
+                      LANES[lane][1] + align_x] = 110
+            _blit(frame, templates["lntail"],
+                  LANES[lane][0] + align_x, tail_y + align_y)
+            _blit(frame, templates["lnhead"],
+                  LANES[lane][0] + align_x, head_y + align_y)
+            writer.write(frame)
+    finally:
+        writer.release()
+
+    lanes = [LaneConfig(
+        index=i, color="white", x_range=x_range, match_x_range=x_range,
+        detection_channel="gray", stage1_threshold=50.0,
+        matching_threshold=0.75, templates=templates)
+        for i, x_range in enumerate(LANES)]
+    config = RunConfig(
+        song_name="synthetic", difficulty="SHD", game="synthetic",
+        skin_name="synthetic", key_mode="2k", display_resolution=(200, 160),
+        template_scale=1.0, video_path=path, fps=FPS, note_speed=1.0,
+        tick_resolution=192, min_bpm=120.0, max_bpm=120.0,
+        playfield_top=10, playfield_bottom=150, line_y=LINE_Y,
+        note_height=NOTE_HEIGHT, note_width=30,
+        trigger_template_y_top=TRIGGER, tail_release_offset_px=0,
+        key_count=2, lanes=lanes, tail_search_y_max=TRIGGER,
+        beat_roi=(10, 10, 20, 20), beat_channel="gray",
+        beat_diff_threshold=10.0,
+        measure_line=MeasureLineConfig(20.0, 130.0, 3, 0, 50.0),
+        pixels_per_frame=SPEED, min_longnote_px=10,
+        alignment_band_y=(140, 145), alignment_max_shift=8,
+        provenance={"fixture": "procedural"})
+
+    ms = lambda frame: frame / FPS * 1000.0
+    truth = {
+        "notes": [
+            {"lane": 0, "type": "tap", "start_ms": ms(50), "end_ms": None,
+             "start_tick": 192, "end_tick": None},
+            {"lane": 1, "type": "tap", "start_ms": ms(80), "end_ms": None,
+             "start_tick": 384, "end_tick": None},
+            {"lane": 0, "type": "longnote", "start_ms": ms(110),
+             "end_ms": ms(200), "start_tick": 576, "end_tick": 1152},
+            {"lane": 1, "type": "tap", "start_ms": ms(230), "end_ms": None,
+             "start_tick": 1344, "end_tick": None},
+        ],
+        "beats_ms": [ms(frame) for frame in BEAT_FRAMES],
+        "barlines_ms": [ms(frame) for frame in BARLINE_FRAMES],
+        "tempo_segments": [{"start_tick": 0, "end_tick": 1536,
+                             "bpm": 120.0, "interpolation": "step"}],
+    }
+    return config, truth

khanwul/EZ2CV-djmax

src/ez2cv/io.py
@@ -15,10 +15,11 @@ from ez2cv.detection.beat import BeatEvent
 from ez2cv.detection.tracking import RawNote
 
 
-RAW_SCHEMA_VERSION = "3.2"
+RAW_SCHEMA_VERSION = "3.3"
+RAW_READABLE_VERSIONS = {"3.1", "3.2", "3.3"}
 CHART_FORMAT = "ez2cv.chart"
-CHART_VERSION = "3.2"
-_READABLE_VERSIONS = {"3.1", "3.2"}
+CHART_VERSION = "3.3"
+CHART_READABLE_VERSIONS = {"3.1", "3.2", "3.3"}
 
 
 def _reject_constant(value: str) -> None:
@@ -28,7 +29,7 @@ def _reject_constant(value: str) -> None:
 def _ms(value: float) -> float:
     if not math.isfinite(value):
         raise ValueError(f"non-finite ms value: {value}")
-    return round(float(value), 3)
+    return float(value)
 
 
 def _tick(value: int) -> int:
@@ -75,6 +76,7 @@ def serialize_raw(raw: RawChart) -> dict:
             "frame_count": int(raw.frame_count),
             "duration_ms": _ms(raw.duration_ms),
             "orphan_tails": int(raw.orphan_tails),
+            "provenance": raw.provenance,
         },
         "notes": [
             {
@@ -85,7 +87,10 @@ def serialize_raw(raw: RawChart) -> dict:
                 "end_ms": _ms(note.end_ms) if note.end_ms is not None else None,
                 "confidence": round(float(note.confidence), 4),
                 "extrapolated": bool(note.extrapolated),
-                "timing_sigma_ms": _ms(note.timing_sigma_ms),
+                "start_sigma_ms": _ms(note.start_sigma_ms),
+                "end_sigma_ms": (_ms(note.end_sigma_ms)
+                                 if note.end_sigma_ms is not None else None),
+                "pairing_status": note.pairing_status,
             }
             for note in raw.notes
         ],
@@ -117,17 +122,20 @@ def read_raw(path: str | Path) -> RawChart:
             source.read_text(encoding="utf-8"),
             parse_constant=_reject_constant,
         )
-        if payload["schema_version"] not in _READABLE_VERSIONS:
+        if payload["schema_version"] not in RAW_READABLE_VERSIONS:
             raise ValueError(
                 f"unsupported schema_version {payload['schema_version']!r}; "
-                f"expected one of {sorted(_READABLE_VERSIONS)!r}")
+                f"expected one of {sorted(RAW_READABLE_VERSIONS)!r}")
         meta = payload["meta"]
         if meta["difficulty"] not in {"NM", "HD", "MX", "SC"}:
             raise ValueError("invalid difficulty")
+        game = meta.get("game", "unknown")
+        if not isinstance(game, str) or not game.strip():
+            raise ValueError("invalid game")
         raw = RawChart(
             song_name=str(meta["song"]),
             difficulty=str(meta["difficulty"]),
-            game=str(meta.get("game", "unknown")),
+            game=game,
             skin_name=str(meta["skin"]),
             key_mode=str(meta["key_mode"]),
             tracks=tuple(TrackMetadata(
@@ -148,6 +156,7 @@ def read_raw(path: str | Path) -> RawChart:
             max_bpm=float(meta["max_bpm"]),
             frame_count=int(meta["frame_count"]),
             orphan_tails=int(meta["orphan_tails"]),
+            provenance=dict(meta.get("provenance", {})),
             notes=[RawNote(
                 lane=int(note["lane"]),
                 type=str(note["type"]),
@@ -157,7 +166,14 @@ def read_raw(path: str | Path) -> RawChart:
                 color=str(note["color"]),
                 confidence=float(note["confidence"]),
                 extrapolated=bool(note["extrapolated"]),
-                timing_sigma_ms=float(note.get("timing_sigma_ms", 0.0)),
+                start_sigma_ms=float(note.get(
+                    "start_sigma_ms", note.get("timing_sigma_ms", 0.0))),
+                end_sigma_ms=(
+                    None if note["end_ms"] is None
+                    else float(note["end_sigma_ms"])
+                    if note.get("end_sigma_ms") is not None
+                    else 0.0 if "end_sigma_ms" not in note else None),
+                pairing_status=str(note.get("pairing_status", "observed")),
             ) for note in payload["notes"]],
             beats=[BeatEvent(
                 frame_index=int(beat["frame_index"]),
@@ -177,14 +193,17 @@ def read_raw(path: str | Path) -> RawChart:
     numeric = [raw.fps, raw.note_speed, raw.min_bpm, raw.max_bpm]
     numeric.extend(note.trigger_ms for note in raw.notes)
     numeric.extend(note.end_ms for note in raw.notes if note.end_ms is not None)
-    numeric.extend(note.timing_sigma_ms for note in raw.notes)
+    numeric.extend(note.start_sigma_ms for note in raw.notes)
+    numeric.extend(note.end_sigma_ms for note in raw.notes
+                   if note.end_sigma_ms is not None)
     numeric.extend(beat.ms for beat in raw.beats)
     numeric.extend(barline.ms for barline in raw.barlines)
     if not all(math.isfinite(value) for value in numeric):
         raise ValueError(f"invalid raw chart {source}: non-finite number")
-    if (not raw.game or raw.fps <= 0 or raw.tick_resolution <= 0
-            or not raw.tracks):
+    if raw.fps <= 0 or raw.tick_resolution != 192 or not raw.tracks:
         raise ValueError(f"invalid raw chart {source}: invalid timing or lanes")
+    if not isinstance(raw.game, str) or not raw.game.strip():
+        raise ValueError(f"invalid raw chart {source}: invalid game")
     if [track.index for track in raw.tracks] != list(range(raw.key_count)):
         raise ValueError(f"invalid raw chart {source}: invalid track indexes")
     if any(track.role not in {"normal", "overlay"} for track in raw.tracks):
@@ -202,8 +221,14 @@ def read_raw(path: str | Path) -> RawChart:
         raise ValueError(f"invalid raw chart {source}: note lane out of range")
     if any(note.type not in {"tap", "longnote"} for note in raw.notes):
         raise ValueError(f"invalid raw chart {source}: unknown note type")
-    if any(note.timing_sigma_ms < 0 for note in raw.notes):
+    if any(note.start_sigma_ms < 0 or (
+            note.end_sigma_ms is not None and note.end_sigma_ms < 0)
+            for note in raw.notes):
         raise ValueError(f"invalid raw chart {source}: negative timing sigma")
+    if any(note.pairing_status not in {
+            "observed", "inferred_tail", "unclosed_head", "short_pair"}
+            for note in raw.notes):
+        raise ValueError(f"invalid raw chart {source}: invalid pairing status")
     if any(note.type == "longnote" and (
             note.end_ms is None or note.end_ms <= note.trigger_ms)
             for note in raw.notes):
@@ -221,6 +246,7 @@ def _note_payload(note: ChartNote, note_id: int) -> dict:
         "start_tick": _tick(note.start_tick),
         "end_tick": _tick(note.end_tick) if note.end_tick is not None else None,
         "off_grid": bool(note.off_grid),
+        "needs_review": bool(note.needs_review),
     }
 
 
@@ -230,11 +256,11 @@ def _bpm_segment_payload(segment: BPMSegment) -> dict:
         "end_tick": _tick(segment.end_tick),
     }
     if segment.is_constant:
-        out.update(bpm=round(float(segment.bpm_start), 4),
+        out.update(bpm=float(segment.bpm_start),
                    interpolation="step")
     else:
-        out.update(bpm_start=round(float(segment.bpm_start), 4),
-                   bpm_end=round(float(segment.bpm_end), 4),
+        out.update(bpm_start=float(segment.bpm_start),
+                   bpm_end=float(segment.bpm_end),
                    interpolation="linear_time")
     return out
 
@@ -293,7 +319,7 @@ def serialize_chart(chart: Chart) -> dict:
         "notes": [
             _note_payload(note, note_id)
             for note_id, note in enumerate(chart.notes)],
-        "analysis": {"stats": chart.stats},
+        "analysis": {"stats": chart.stats, "notes": chart.diagnostics},
     }
 
 
@@ -304,7 +330,7 @@ def read_chart(path: str | Path) -> dict:
         chart = json.loads(source.read_text(encoding="utf-8"),
                            parse_constant=_reject_constant)
         if (chart["format"] != CHART_FORMAT
-                or chart["version"] not in _READABLE_VERSIONS):
+                or chart["version"] not in CHART_READABLE_VERSIONS):
             raise ValueError(
                 f"unsupported chart {chart.get('format')!r} "
                 f"version {chart.get('version')!r}")
@@ -325,7 +351,7 @@ def read_chart(path: str | Path) -> dict:
         meters = timing["meter_events"]
         if lane_count <= 0 or resolution <= 0:
             raise ValueError("invalid lanes or tick resolution")
-        if not isinstance(meta["game"], str) or not meta["game"]:
+        if not isinstance(meta["game"], str) or not meta["game"].strip():
             raise ValueError("invalid game")
         if [track["index"] for track in tracks] != list(range(lane_count)):
             raise ValueError("invalid track indexes")
@@ -360,6 +386,8 @@ def read_chart(path: str | Path) -> dict:
             if meter["numerator"] <= 0 or meter["denominator"] <= 0:
                 raise ValueError("invalid meter event")
         for note in notes:
+            if chart["version"] in {"3.1", "3.2"}:
+                note.setdefault("needs_review", False)
             if not 0 <= note["lane"] < lane_count:
                 raise ValueError("note lane out of range")
             if (note["end_tick"] is not None
@@ -367,6 +395,8 @@ def read_chart(path: str | Path) -> dict:
                 raise ValueError("invalid longnote end")
             if not isinstance(note["off_grid"], bool):
                 raise ValueError("off_grid must be boolean")
+            if not isinstance(note["needs_review"], bool):
+                raise ValueError("needs_review must be boolean")
         numeric = [meta["duration_ms"], timing["tick_zero_ms"]]
         if not all(math.isfinite(float(value)) for value in numeric):
             raise ValueError("non-finite chart metadata")

tests/synthetic_video.py
@@ -1,0 +1,132 @@
+"""Procedural, copyright-free video fixture for the public end-to-end test."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+from ez2cv.config import LaneConfig, MeasureLineConfig, RunConfig
+
+
+FPS = 60.0
+SPEED = 4.0
+TRIGGER = 120
+LINE_Y = 130
+NOTE_HEIGHT = 10
+LANES = ((60, 90), (90, 120))
+BEAT_FRAMES = tuple(range(20, 261, 30))
+BARLINE_FRAMES = (20, 140, 260)
+
+
+def _sprite(kind: str) -> np.ndarray:
+    image = np.full((NOTE_HEIGHT, 30, 3), 180, dtype=np.uint8)
+    if kind == "note":
+        image[:, 12:18] = 250
+        image[2:8, 14:16] = 40
+    elif kind == "lnhead":
+        image[:, 4:10] = 245
+        image[2:8, 6:8] = 30
+    else:
+        image[:, 20:26] = 245
+        image[2:8, 22:24] = 30
+    return image
+
+
+def _blit(frame: np.ndarray, sprite: np.ndarray, x: int, y: int) -> None:
+    h, w = sprite.shape[:2]
+    if 0 <= y <= frame.shape[0] - h:
+        frame[y:y + h, x:x + w] = sprite
+
+
+def make_fixture(path: Path, alignment_offset=(0, 0)) -> tuple[RunConfig, dict]:
+    align_x, align_y = alignment_offset
+    templates = {kind: _sprite(kind) for kind in ("note", "lnhead", "lntail")}
+    writer = cv2.VideoWriter(
+        str(path), cv2.VideoWriter_fourcc(*"FFV1"), FPS, (200, 160))
+    if not writer.isOpened():
+        raise RuntimeError("FFV1 video writer unavailable")
+
+    taps = ((0, 50), (1, 80), (1, 230))
+    longnote = (0, 110, 200)       # lane, head crossing, release frame
+    try:
+        for frame_index in range(285):
+            frame = np.zeros((160, 200, 3), dtype=np.uint8)
+            frame[140 + align_y:145 + align_y,
+                  LANES[0][0] + align_x:LANES[-1][1] + align_x + 1, 0] = 220
+            if frame_index - 1 in BEAT_FRAMES:
+                frame[10 + align_y:20 + align_y,
+                      10 + align_x:20 + align_x] = 255
+            for crossing in BARLINE_FRAMES:
+                y = int(round(LINE_Y + SPEED * (frame_index - crossing)))
+                if 10 <= y < 150:
+                    frame[y + align_y,
+                          LANES[0][0] + align_x:
+                          LANES[-1][1] + align_x] = 80
+            for lane, crossing in taps:
+                y = int(round(TRIGGER + SPEED * (frame_index - crossing)))
+                _blit(frame, templates["note"],
+                      LANES[lane][0] + align_x, y + align_y)
+
+            lane, head_crossing, release = longnote
+            tail_crossing = release - NOTE_HEIGHT / SPEED
+            head_y = int(round(TRIGGER + SPEED * (frame_index - head_crossing)))
+            tail_y = int(round(TRIGGER + SPEED * (frame_index - tail_crossing)))
+            top, bottom = max(10, tail_y), min(150, head_y + NOTE_HEIGHT)
+            if top < bottom:
+                frame[top + align_y:bottom + align_y,
+                      LANES[lane][0] + align_x:
+                      LANES[lane][1] + align_x] = 110
+            _blit(frame, templates["lntail"],
+                  LANES[lane][0] + align_x, tail_y + align_y)
+            _blit(frame, templates["lnhead"],
+                  LANES[lane][0] + align_x, head_y + align_y)
+            writer.write(frame)
+    finally:
+        writer.release()
+
+    lanes = [LaneConfig(
+        index=i, name=f"K{i + 1}", role="normal", input_type="key",
+        color="white", template_set="synthetic",
+        allowed_types=frozenset(("tap", "longnote")),
+        x_range=x_range, match_x_range=x_range,
+        note_height=NOTE_HEIGHT, trigger_y_top=TRIGGER,
+        timing_offset_px=0, tail_release_offset_px=0,
+        tail_search_y_max=TRIGGER, min_longnote_px=10,
+        include_in_consensus=True,
+        detection_channel="gray", stage1_threshold=50.0,
+        matching_threshold=0.75, templates=templates)
+        for i, x_range in enumerate(LANES)]
+    config = RunConfig(
+        song_name="synthetic", difficulty="SC", game="synthetic",
+        skin_name="synthetic", key_mode="2k", display_resolution=(200, 160),
+        template_scale=1.0, video_path=path, fps=FPS, note_speed=1.0,
+        tick_resolution=192, min_bpm=120.0, max_bpm=120.0,
+        playfield_top=10, playfield_bottom=150, line_y=LINE_Y,
+        normal_lane_count=2, lanes=lanes,
+        beat_roi=(10, 10, 20, 20), beat_channel="gray",
+        beat_diff_threshold=10.0,
+        measure_line=MeasureLineConfig(20.0, 130.0, 3, 0, 50.0),
+        pixels_per_frame=SPEED,
+        alignment_band_y=(140, 145), alignment_max_shift=8,
+        provenance={"fixture": "procedural"})
+
+    ms = lambda frame: frame / FPS * 1000.0
+    truth = {
+        "notes": [
+            {"lane": 0, "type": "tap", "start_ms": ms(50), "end_ms": None,
+             "start_tick": 192, "end_tick": None},
+            {"lane": 1, "type": "tap", "start_ms": ms(80), "end_ms": None,
+             "start_tick": 384, "end_tick": None},
+            {"lane": 0, "type": "longnote", "start_ms": ms(110),
+             "end_ms": ms(200), "start_tick": 576, "end_tick": 1152},
+            {"lane": 1, "type": "tap", "start_ms": ms(230), "end_ms": None,
+             "start_tick": 1344, "end_tick": None},
+        ],
+        "beats_ms": [ms(frame) for frame in BEAT_FRAMES],
+        "barlines_ms": [ms(frame) for frame in BARLINE_FRAMES],
+        "tempo_segments": [{"start_tick": 0, "end_tick": 1536,
+                             "bpm": 120.0, "interpolation": "step"}],
+    }
+    return config, truth